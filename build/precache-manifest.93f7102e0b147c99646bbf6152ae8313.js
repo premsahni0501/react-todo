4be7d549e0a9639e944dff998a5f2ebc6bed1bab
Prem Chand <premsahni0501@gmail.com>
2019-05-05 21:18:25 +0530

    build folder

---
+++ build/precache-manifest.93f7102e0b147c99646bbf6152ae8313.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd14c5843982fe4acc433933107703e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "992a4ee49914503ae9d3",
+    "url": "/static/css/main.b44a4e0c.chunk.css"
+  },
+  {
+    "revision": "efcdc59a62fac625810a",
+    "url": "/static/js/2.c55990cf.chunk.js"
+  },
+  {
+    "revision": "992a4ee49914503ae9d3",
+    "url": "/static/js/main.79cbd5df.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
